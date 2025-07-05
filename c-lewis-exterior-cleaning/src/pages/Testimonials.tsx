4fe18e4cc144780077a252ca324ba11f4679b60f
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-05 13:54:43 +0100

    chore: imported hero image in testimonials page, pass path to hero component

--- c-lewis-exterior-cleaning/src/pages/Testimonials.tsx
+++ c-lewis-exterior-cleaning/src/pages/Testimonials.tsx
@@ -7,6 +7,7 @@ import roofCleanAfter2 from '@/assets/images/before-after/roof-after-2.jpg';
 import roofCleanBefore from '@/assets/images/before-after/before1.jpg';
 import roofCleanAfter from '@/assets/images/before-after/after1.jpg';
 import { CustomerQuote } from "@/components/testimonials-page/CustomerQuote";
+import testimonials from '@/assets/images/roof.jpg';
 
 export const Testimonials = () => {
     
@@ -16,7 +17,7 @@ export const Testimonials = () => {
 
     return (
         <main>
-            <Hero title="testimonials" subtitle="Real Stories. Real Results." />
+            <Hero title="testimonials" image={testimonials} subtitle="Real Stories. Real Results." />
             <CustomerQuote
                 quote={`"Superb job carried out by Chad. Reasonably priced, reliable and courteous. we would definitely recommend him."`}
                 author={"- MORAG BAYLEY"}
