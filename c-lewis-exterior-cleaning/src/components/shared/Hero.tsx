4c72c2a9ea55a81259ed68e24b87f9ed7c6e8803
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-05 11:02:59 +0100

    chore: added a placeholder until we decide on image in hero, simplified props

--- c-lewis-exterior-cleaning/src/components/shared/Hero.tsx
+++ c-lewis-exterior-cleaning/src/components/shared/Hero.tsx
@@ -1,20 +1,19 @@
-import portfolio from '../../assets/images/vans/van6.jpeg';
-import testimonials from '../../assets/images/roof.jpg';
-
 interface HeroProps {
-    title: 'portfolio' | 'testimonials';
+    title: string;
     subtitle: string;
+    image?: string;
 }
 
-const heroImages = {
-    'testimonials': testimonials,
-    'portfolio': portfolio,
-}
-
-export const Hero = ({ title, subtitle }: HeroProps) => {
+export const Hero = ({ title, subtitle, image }: HeroProps) => {
+    
     return (
         <section className="relative w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px]">
-            <img src={heroImages[title]} alt={title} className="w-full h-full object-cover" />
+            {image ? (
+                <img src={image} alt={title} className="w-full h-full object-cover" />
+            ) : (
+                <div className="w-full h-full bg-gray-100">
+                </div>
+            )}
             <div className="absolute inset-0 bg-black/30"></div>
             <div className="absolute inset-0 flex flex-col gap-4 items-center justify-center">
                 <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold text-white text-center">
