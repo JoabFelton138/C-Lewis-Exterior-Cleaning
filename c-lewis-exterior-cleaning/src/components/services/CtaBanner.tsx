7652afd53e471d85640e0ca486cf41fc883c5a70
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-05 16:34:21 +0100

    feat: cta banner to be included in service pages

---
+++ c-lewis-exterior-cleaning/src/components/services/CtaBanner.tsx
@@ -0,0 +1,51 @@
+import { Phone } from "lucide-react";
+import { MessageCircle } from "lucide-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useQuoteNavigation } from "../utils/navigation";
+
+export const CtaBanner = () => {
+
+    const navigate = useNavigate();
+    
+    const styles = {
+        container: "p-20 text-white flex flex-row text-center items-center justify-center",
+        icon: "w-6 h-6 fill-current",
+        text: "text-2xl font-semibold leading-relaxed ml-3",
+        leftAnimation: "animate-on-scroll-left",
+        rightAnimation: "animate-on-scroll-right",
+    }
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("in-view");
+                }
+            })
+        })
+        document.querySelectorAll('.animate-on-scroll-left, .animate-on-scroll-right').forEach(el => observer.observe(el))
+        return () => observer.disconnect();
+    }, []);
+
+    const navigateToQuoteForm = useQuoteNavigation();
+
+    return (
+        <section className="w-full">
+            <div className="grid grid-cols-1 md:grid-cols-2">
+                <div className={`bg-sky-400 ${styles.container}`}>
+                    <div className={`${styles.leftAnimation} flex items-center`}>
+                        <Phone className={`${styles.icon} rotate-260`}/>
+                        <h2 className={`${styles.text}`}>Call Us at 07391 853 506</h2>
+                    </div>
+                </div>
+                <a onClick={navigateToQuoteForm} className={`bg-sky-700 ${styles.container} cursor-pointer block`}>
+                    <div className={`${styles.rightAnimation} flex items-center`}>
+                        <MessageCircle className={`${styles.icon}`}/>
+                        <h2 className={`${styles.text}`}>Send Us a Message</h2>
+                    </div>
+                </a>
+            </div>
+        </section>
+    );
+}
