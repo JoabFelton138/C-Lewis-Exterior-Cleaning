c2aa0c3fea1c7a7053a561acdd2c85150038d67e
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-06 15:48:18 +0100

    chore: externalised data structure that will contain areas for service along with url for map in iframe

---
+++ c-lewis-exterior-cleaning/src/data/service-areas.ts
@@ -0,0 +1,26 @@
+export const serviceAreas = {
+    roofsAndPatios: {
+        areas:[
+            "Birmingham",
+            "Coventry",
+            "Wolverhampton",
+            "Solihull",
+            "Dudley",
+            "Walsall",
+            "West Bromwich",
+            "Stoke‑on‑Trent",
+            "Tamworth",
+            "Newcastle‑under‑Lyme",
+            "Burton‑upon‑Trent",
+            "Stafford",
+            "Lichfield",
+            "Telford",
+            "Shrewsbury",
+            "Oswestry",
+            "Bridgnorth",
+            "Newport",
+            "Ludlow"
+        ],
+        mapSrc: "https://www.google.com/maps/d/u/0/embed?mid=1zXpnVO-xL4xv3jSC4yqNbieisF2HHZk&ehbc=2E312F&noprof=1"
+    }
+}
