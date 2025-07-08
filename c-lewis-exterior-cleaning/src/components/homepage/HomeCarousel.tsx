4425da3e618ec3892f652efb55917e55948dfae3
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-08 14:08:38 +0100

    chore: externalised array of objects containing slide data

--- c-lewis-exterior-cleaning/src/components/homepage/HomeCarousel.tsx
+++ c-lewis-exterior-cleaning/src/components/homepage/HomeCarousel.tsx
@@ -8,43 +8,14 @@ import {
 } from "../ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import Fade from "embla-carousel-fade";
-import van5 from "../../assets/images/carousel/van5-min.jpg";
-import cc from "../../assets/images/carousel/cc-min.jpg";
-import pw3 from "../../assets/images/carousel/pw3-min.jpg";
-import gutter from "../../assets/images/carousel/gutter.jpg";
 import { Button } from "../ui/button";
 import { useState, useEffect } from "react";
+import { carouselSlides } from "@/data/carousel-slides";
 
 export const HomeCarousel = () => {
   const [activeSlide, setActiveSlide] = useState(0);
   const [isTransitioning, setIsTransitioning] = useState(false);
-
-  const slides = [
-    {
-      src: van5,
-      alt: "Van",
-      title: "EXPERT EXTERIOR CLEANING",
-      subtitle: "COVERING STAFFORDSHIRE, SHROPSHIRE, WEST MIDLANDS & MORE",
-    },
-    {
-      src: cc,
-      alt: "Conservatory Cleaning",
-      title: "LET ME LIGHT UP YOUR HOME",
-      subtitle: "CONSERVATORY & SOLAR PANEL CLEANING",
-    },
-    {
-      src: pw3,
-      alt: "Pressue Washing",
-      title: "LEAVE THE PRESSURE TO ME",
-      subtitle: "PRESSURE WASHING SERVICES",
-    },
-    {
-      src: gutter,
-      alt: "Gutter Cleaning",
-      title: "CLEAN GUTTERS, CLEAN MIND",
-      subtitle: "GUTTERS & FASCIA CLEANING",
-    },
-  ];
+  const slides = carouselSlides;
 
   useEffect(() => {
 
---
+++ c-lewis-exterior-cleaning/src/data/carousel-slides.ts
@@ -0,0 +1,31 @@
+import van5 from "../assets/images/carousel/van5-min.jpg";
+import cc from "../assets/images/carousel/cc-min.jpg";
+import pw3 from "../assets/images/carousel/pw3-min.jpg";
+import gutter from "../assets/images/carousel/gutter.jpg";
+
+export const carouselSlides = [
+    {
+      src: van5,
+      alt: "Van",
+      title: "EXPERT EXTERIOR CLEANING",
+      subtitle: "COVERING STAFFORDSHIRE, SHROPSHIRE, WEST MIDLANDS & MORE",
+    },
+    {
+      src: cc,
+      alt: "Conservatory Cleaning",
+      title: "LET ME LIGHT UP YOUR HOME",
+      subtitle: "CONSERVATORY & SOLAR PANEL CLEANING",
+    },
+    {
+      src: pw3,
+      alt: "Pressue Washing",
+      title: "LEAVE THE PRESSURE TO ME",
+      subtitle: "PRESSURE WASHING SERVICES",
+    },
+    {
+      src: gutter,
+      alt: "Gutter Cleaning",
+      title: "CLEAN GUTTERS, CLEAN MIND",
+      subtitle: "GUTTERS & FASCIA CLEANING",
+    },
+  ];
