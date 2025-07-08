f67c21339c0430dc3a22c0f781b80d3e4ed4b339
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-08 12:38:35 +0100

    style: improved responsiveness of text within <Footer/>

--- c-lewis-exterior-cleaning/src/components/shared/Footer.tsx
+++ c-lewis-exterior-cleaning/src/components/shared/Footer.tsx
@@ -9,32 +9,38 @@ export const Footer = () => {
             <div className="grid grid-cols-1 sm:grid-cols-3 max-w-5xl mx-auto px-4 py-12 text-center gap-8">
 
                 <div className="space-y-4">
-                    <h3 className="text-2xl sm:text-lg font-bold sm:font-semibold">
-                        C. Lewis Exterior Cleaning
+                <h3 className="text-base sm:text-sm lg:text-lg title-style">
+                C. Lewis Exterior Cleaning
                     </h3>
-                    <p className="text-sm">
+                    <p className="text-xs sm:text-sm">
                         Professional exterior cleaning services for your home and business.
                     </p>
                 </div>
                 
                 <div className="space-y-4">
-                    <h4 className="text-lg font-semibold ">
-                        Contact
-                    </h4>
+                <h3 className="text-base sm:text-sm lg:text-lg title-style">
+                Contact
+                    </h3>
                     <div className="space-y-2">
                         <div className="flex items-center space-x-2 justify-center">
-                            <Phone className="h-4 w-4"/>
-                            <span>07391 853 506</span>
+                            <Phone className="h-3 sm:h-4 w-3 sm:w-4"/>
+                            <span className="text-xs sm:text-sm">
+                                07391 853 506
+                            </span>
                         </div>
                         <div className="flex items-center space-x-2 justify-center">
-                            <Mail className="h-4 w-4"/>
-                            <span>c.lewis.pws@hotmail.com</span>
+                            <Mail className="h-3 sm:h-4 w-3 sm:w-4"/>
+                            <span className="text-xs sm:text-sm">
+                                c.lewis.pws@hotmail.com
+                            </span>
                         </div>
                     </div>
                 </div>
 
                 <div className="space-y-4">
-                    <h4 className="text-lg font-semibold">Follow Us</h4>
+                    <h3 className="text-base sm:text-sm lg:text-lg title-style">
+                        Follow Us
+                    </h3>
                     <div className="flex space-x-4 justify-center">
                         <SocialIcons background={hasBackground} title="facebook" />
                         <SocialIcons background={hasBackground} title="instagram" />
@@ -44,7 +50,7 @@ export const Footer = () => {
                 </div>
             
             <div>
-                <div className="border-t border-gray-200 p-6 text-center text-sm">
+                <div className="border-t border-gray-200 p-6 text-center text-xs sm:text-sm">
                     <p>
                         &copy; 2025 C.Lewis Exterior Cleaning. All rights reserved.
                     </p>
