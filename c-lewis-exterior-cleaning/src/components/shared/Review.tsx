9a70052236e77a34fd60a73b8c91318b188c301f
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-18 17:47:18 +0100

    feat: review grid for testimonials page

--- c-lewis-exterior-cleaning/src/components/shared/Review.tsx
+++ c-lewis-exterior-cleaning/src/components/shared/Review.tsx
@@ -10,38 +10,70 @@ interface ReviewProps {
   author: string;
   image?: string;
   url: string;
+  isLarge?: boolean;
 }
 
-export const Review = ({ ...ReviewProps }: ReviewProps) => {
-  return (
-    <Card 
-        className="bg-white p-5 h-full w-[320px] md:w-full 
-        cursor-pointer rounded-lg gap-1 hover:-translate-y-2 transition-all duration-300"
-        onClick={() => window.location.href = ReviewProps.url}>
-      <CardHeader className="px-0">
-        <CardTitle className="flex justify-between">
-          <div className="flex items-center gap-3">
-            <Avatar className="w-10 h-10">
-              <AvatarImage src={ReviewProps.image} className="object-cover" />
-              <AvatarFallback>
-                {ReviewProps.author.charAt(0)}
-              </AvatarFallback>
-            </Avatar>
-            <div>
-              <h3 className="text-sm font-bold gap-1">{ReviewProps.author}</h3>
-              <div className="flex gap-">
-                {[...Array(ReviewProps.rating)].map((_, i) => (
-                  <Star key={i} className="w-4 h-4 fill-yellow-400 text-yellow-400" />
-                ))}
+export const Review = ({ isLarge = false,...ReviewProps}: ReviewProps) => {
+    
+    const styles = {
+      card: isLarge 
+        ? "p-5 sm:p-6 lg:p-8 w-full max-w-[320px] sm:max-w-none h-auto min-h-[250px] sm:min-h-[275px] md:min-h-[300px] lg:min-h-[350px] gap-2 mx-auto sm:mx-0"        
+        : "p-5 w-[320px] md:w-full h-full gap-1 hover:-translate-y-2 transition-all duration-300",
+      avatar: isLarge 
+        ? "w-10 h-10 lg:w-12 lg:h-12" 
+        : "w-10 h-10",
+      author: isLarge
+        ? "sm:text-sm md:text-base lg:text-lg" 
+        : "text-xs lg:text-sm",
+      stars: isLarge
+        ? "w-4 h-4 sm:w-5 sm:h-5"
+        : "w-4 h-4",
+      starsContainer: isLarge
+        ? "mt-1"
+        : "",
+      googleLogo: isLarge 
+        ? "w-5 h-5 sm:w-6 sm:h-6 lg:w-7 lg:h-7"
+        : "w-6 h-6",
+      comment: isLarge
+        ? "text-sm md:text-base"
+        : "text-xs lg:text-sm",
+      cardContent: isLarge
+          ? ""
+          : "mt-1"
+    }
+
+    return (
+      <Card 
+          className={`bg-white cursor-pointer rounded-lg ${styles.card}`}
+          onClick={() => window.location.href = ReviewProps.url}>
+        <CardHeader className="px-0">
+          <CardTitle className="flex justify-between">
+            <div className="flex items-center gap-4">
+              <Avatar className={`${styles.avatar} flex-shrink-0`}>
+                <AvatarImage src={ReviewProps.image} className="object-cover w-full h-full" />
+                <AvatarFallback>
+                  {ReviewProps.author.charAt(0)}
+                </AvatarFallback>
+              </Avatar>
+              <div>
+                <h3 className={`font-bold gap-1 ${styles.author}`}>
+                    {ReviewProps.author}
+                </h3>
+                <div className={`flex gap-1 ${styles.starsContainer}`}>
+                  {[...Array(ReviewProps.rating)].map((_, i) => (
+                    <Star key={i} className={`${styles.stars} fill-yellow-400 text-yellow-400`} />
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-          <img src={google} alt="Google" className="w-6 h-6 mb-[25px]" />
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="px-0">
-        <p className="text-xs lg:text-sm text-gray-500">{ReviewProps.comment}</p>
-      </CardContent>
-    </Card>
-  );
+            <img src={google} alt="Google" className={`${styles.googleLogo}`} />
+          </CardTitle>
+        </CardHeader>
+        <CardContent className={`px-0 ${styles.cardContent}`}>
+          <p className={`text-gray-500  ${styles.comment}`}>
+            {ReviewProps.comment}
+          </p>
+        </CardContent>
+      </Card>
+    );
 };
--- c-lewis-exterior-cleaning/src/pages/Testimonials.tsx
+++ c-lewis-exterior-cleaning/src/pages/Testimonials.tsx
@@ -1,59 +1,27 @@
 import { Hero } from "@/components/shared/Hero";
-
-import { useEffect } from "react";
-import { BeforeAfterTestimonial } from "@/components/testimonials-page/BeforeAfterTestimonial";
-import roofCleanBefore2 from '@/assets/images/before-after/roof-cleaning/roof-before-2.jpg';
-import roofCleanAfter2 from '@/assets/images/before-after/roof-cleaning/roof-after-2.jpg';
-import roofCleanBefore from '@/assets/images/before-after/before1.jpg';
-import roofCleanAfter from '@/assets/images/before-after/after1.jpg';
-import { CustomerQuote } from "@/components/testimonials-page/CustomerQuote";
 import testimonials from '@/assets/images/roof.jpg';
+import {reviews} from '../data/testimonial-section-reviews';
+import { Review } from "@/components/shared/Review";
 
 export const Testimonials = () => {
     
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
-
     return (
         <main>
             <Hero title="testimonials" image={testimonials} subtitle="Real Stories. Real Results." />
-            <CustomerQuote
-                quote={`"Superb job carried out by Chad. Reasonably priced, reliable and courteous. we would definitely recommend him."`}
-                author={"- MORAG BAYLEY"}
-            />
-            <section className="pt-20 px-4">            
-                <BeforeAfterTestimonial 
-                    layout="left-to-right"
-                    beforeImage={roofCleanBefore2}
-                    afterImage={roofCleanAfter2}
-                    beforeAlt="Roof before cleaning"
-                    afterAlt="Roof after cleaning"
-                    text={"Amazing transformation of roof and exterior area of property. Absolutely delighted with the results- thank you!!! Chad worked diligently, was punctual, polite & trustworthy being left alone with the property whilst the work was completed. Totally recommend his services & you too will be delighted with the result."} 
-                    author={"HELEN ROBERTS"}
-                />
-            </section>
-            <section className="pt-10 px-4">
-                <BeforeAfterTestimonial 
-                    layout="right-to-left"
-                    beforeImage={roofCleanBefore}
-                    afterImage={roofCleanAfter}
-                    beforeAlt="Roof before cleaning"
-                    afterAlt="Roof after cleaning"
-                    text={"Amazing transformation of roof and exterior area of property. Absolutely delighted with the results- thank you!!! Chad worked diligently, was punctual, polite & trustworthy being left alone with the property whilst the work was completed. Totally recommend his services & you too will be delighted with the result."} 
-                    author={"HELEN ROBERTS"}
-                />
-            </section>
-            <section className="pt-10 pb-20 px-4">
-                <BeforeAfterTestimonial 
-                    layout="left-to-right"
-                    beforeImage={roofCleanBefore}
-                    afterImage={roofCleanAfter}
-                    beforeAlt="Roof before cleaning"
-                    afterAlt="Roof after cleaning"
-                    text={"Amazing transformation of roof and exterior area of property. Absolutely delighted with the results- thank you!!! Chad worked diligently, was punctual, polite & trustworthy being left alone with the property whilst the work was completed. Totally recommend his services & you too will be delighted with the result."} 
-                    author={"HELEN ROBERTS"}
-                />
+            <section className="py-12 sm:py-16 lg:py-24 px-12 md:px-14 lg:px-16 max-w-6xl mx-auto"> 
+                <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5">
+                    {reviews.map((review) => (
+                        <Review
+                            key={review.author}
+                            rating={review.rating}
+                            comment={review.comment}
+                            author={review.author}
+                            url={review.url}
+                            isLarge={true}
+                            image={review.image}
+                        />
+                    ))}
+                </div>
             </section>
         </main>
     );
