6979d67bd8c3ac009d4ac345f682207dfcb01a8c
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-18 20:45:36 +0100

    feat: load more feature, enabling user to load 3 more reviews on click

--- c-lewis-exterior-cleaning/src/pages/Testimonials.tsx
+++ c-lewis-exterior-cleaning/src/pages/Testimonials.tsx
@@ -2,25 +2,37 @@ import { Hero } from "@/components/shared/Hero";
 import testimonials from '@/assets/images/roof.jpg';
 import {reviews} from '../data/testimonial-section-reviews';
 import { Review } from "@/components/shared/Review";
+import { useState } from "react";
+import { ResponsiveButton } from "@/components/shared/ResponsiveButton";
 
 export const Testimonials = () => {
+    const [displayCount, setDisplayCount] = useState(3);
+    const reviewsToShow = reviews.slice(0, displayCount);
+
+    const loadMore = () => {
+        setDisplayCount(prev => prev + 3)
+    }
     
     return (
         <main>
             <Hero title="testimonials" image={testimonials} subtitle="Real Stories. Real Results." />
             <section className="py-12 sm:py-16 lg:py-24 px-12 md:px-14 lg:px-16 max-w-6xl mx-auto"> 
                 <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5">
-                    {reviews.map((review) => (
-                        <Review
+                    {reviewsToShow.map((review) => (
+                        <div
                             key={review.author}
-                            rating={review.rating}
-                            comment={review.comment}
-                            author={review.author}
-                            url={review.url}
-                            isLarge={true}
-                            image={review.image}
-                        />
+                            className={`animate-slide-up active`}
+                        >
+                            <Review
+                                {...review}
+                                isLarge={true}
+                            />
+                        </div>
                     ))}
+          
+                </div>
+                <div className="flex mt-8 justify-center gap-4">
+                    <ResponsiveButton text={"LOAD MORE"} onClick={loadMore}/>
                 </div>
             </section>
         </main>
