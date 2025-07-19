0f70dfcfac0c2a2505877e7c19721e03db684a34
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-19 13:37:04 +0100

    feat: ability to remove row on click with slide left animation

--- c-lewis-exterior-cleaning/src/pages/Testimonials.tsx
+++ c-lewis-exterior-cleaning/src/pages/Testimonials.tsx
@@ -8,13 +8,21 @@ import { ResponsiveButton } from "@/components/shared/ResponsiveButton";
 export const Testimonials = () => {
     const [displayCount, setDisplayCount] = useState<number>(3);
     const reviewsToShow = reviews.slice(0, displayCount);
-
+    const [isRemoving, setIsRemoving] = useState<boolean>(false);
+    const [removingStartIndex, setRemovingStartIndex] = useState<number>(0)
+    
     const loadMore = () => {
         setDisplayCount(prev => prev + 3)
     }
 
     const loadLess = () => {
-        setDisplayCount(prev => prev - 3);
+        setIsRemoving(true);
+        setRemovingStartIndex(displayCount - 3);
+        
+        setTimeout(() => {
+            setDisplayCount(prev => prev - 3);
+            setIsRemoving(false);
+        }, 300);
     }
     
     return (
@@ -22,10 +30,10 @@ export const Testimonials = () => {
             <Hero title="testimonials" image={testimonials} subtitle="Real Stories. Real Results." />
             <section className="py-12 sm:py-16 lg:py-24 px-12 md:px-14 lg:px-16 max-w-6xl mx-auto"> 
                 <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5">
-                    {reviewsToShow.map((review) => (
+                    {reviewsToShow.map((review, index) => (
                         <div
                             key={review.author}
-                            className={`animate-slide-up-fast active`}
+                            className={`${isRemoving && index >= removingStartIndex ? 'animate-slide-out-left' : 'animate-slide-up-fast'} active`}
                         >
                             <Review
                                 {...review}
