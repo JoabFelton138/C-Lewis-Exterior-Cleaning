0e6646e43f5001d922c4f12c7ef6de55a07586b0
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-20 18:50:32 +0100

    feat: header section for testimonials page

--- c-lewis-exterior-cleaning/src/pages/Testimonials.tsx
+++ c-lewis-exterior-cleaning/src/pages/Testimonials.tsx
@@ -4,6 +4,8 @@ import {reviews} from '../data/testimonial-section-reviews';
 import { Review } from "@/components/shared/Review";
 import { useState } from "react";
 import { ResponsiveButton } from "@/components/shared/ResponsiveButton";
+import googleLogo from '@/assets/images/logo.svg';
+import { Star } from "lucide-react";
 
 export const Testimonials = () => {
     const [displayCount, setDisplayCount] = useState<number>(3);
@@ -29,6 +31,38 @@ export const Testimonials = () => {
         <main>
             <Hero title="testimonials" image={testimonials} subtitle="Real Stories. Real Results." />
             <section className="py-12 sm:py-16 lg:py-24 px-12 md:px-14 lg:px-16 max-w-6xl mx-auto"> 
+                <header className="pb-10 flex flex-col xl:flex-row justify-between">
+                    <div className="max-w-2xl flex flex-col gap-4 mx-auto xl:mx-0 text-center xl:text-left">
+                        <h2 className="text-gray-800 title-style text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+                            Find out What our Clients say
+                        </h2>
+                        <p className="text-sm md:text-base text-gray-600">
+                            Since we began, we've helped transform over 100 properties and make homes shine with pride.
+                            From driveways to facades, we're delighted that our work takes centre stage in communities across the West Midlands, helping homes look their absolute best every day.
+                        </p>
+                    </div>
+                    <div className='flex flex-col lg:flex-row items-center justify-center gap-12'>
+                    <div className='flex flex-col gap-2 items-center mt-6 xl:mt-0'>
+                        <h3 className="text-black title-style text-sm sm:text-sm md:text-lg lg:text-xl font-bold">  
+                            EXCELLENT
+                        </h3>
+                        <div className='flex gap-0 sm:gap-1'>
+                            {[...Array(5)].map((_, i) => (
+                                <Star key={i} 
+                                    className="w-4 h-4 sm:w-6 sm:h-6 fill-yellow-400 text-yellow-400"
+                                />
+                            ))}
+                        </div>
+                        <p className='text-black text-xs sm:text-sm'>
+                            Based on <span className="font-bold text-sm sm:text-lg">60+ reviews</span>
+                        </p>
+                        <img src={googleLogo} 
+                            alt="Google Logo" 
+                            className="w-30"
+                        />
+                    </div>
+                </div>
+                </header>
                 <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5">
                     {reviewsToShow.map((review, index) => (
                         <div
@@ -43,7 +77,7 @@ export const Testimonials = () => {
                     ))}
           
                 </div>
-                <div className="flex mt-8 justify-center gap-4">
+                <div className="flex mt-10 justify-center gap-4">
                     {displayCount < reviews.length &&
                         <ResponsiveButton text={"LOAD MORE"} onClick={loadMore}/>
                     }
