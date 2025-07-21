641d5b4003860341f5feb732669b1c7f2c7b0fdd
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-21 19:02:17 +0100

    feat: new images for portfolio grid, removed href prop

--- c-lewis-exterior-cleaning/src/data/portfolio-images.ts
+++ c-lewis-exterior-cleaning/src/data/portfolio-images.ts
@@ -1,59 +1,52 @@
-import gutter1 from '../assets/images/gallery/gutter1.jpeg'
-import pw1 from '../assets/images/gallery/pw12.jpg'
-import pw2 from '../assets/images/pw/pw6.jpg'
-import pw3 from '../assets/images/pw/pw2.jpg'
-import wc1 from '../assets/images/wc/wc1.jpg'
-import wc from '../assets/images/gallery/bwc.jpg'
-import wc2 from '../assets/images/wc/wc7.jpg'
-import wc3 from '../assets/images/wc/wc2fin.jpg'
+import rc1 from '../assets/images/before-after/after1.jpg';
+import dw1 from '../assets/images/portfolio/dw1.jpg';
+import rc2 from '../assets/images/portfolio/rc2.jpg';
+import rc3 from '../assets/images/before-after/roof-cleaning/roof-after-3.jpg';
+import rc4 from '../assets/images/portfolio/rc3.jpg';
+import rc5 from '../assets/images/portfolio/rc5.jpg';
+import dw2 from '../assets/images/portfolio/dw2.jpg';
+import dw3 from '../assets/images/portfolio/dw3.jpg';
+
 
 export const portfolioImages = [
     {
-        href: "/",
-        image: wc,
-        title: "Window Cleaning",
-        description: `We'll put a description of the job here`
+        image: rc1,
+        title: "Roof Clean in telford",
+        description: `Amazing outcome! Revealing the true colours of the tiling really boosted the properties appearance.`
     },
     {
-        href: "/",
-        image: pw3,
-        title: "Pressure Washing",
-        description: `We'll put a description of the job here`
+        image: dw1,
+        title: "driveway in wheaton aston",
+        description: `Pressure washed and re-sanded. This one was really slippery beforehand and quite a hazard due to the slope. Now nice and safe for a lovely cutomer!`
     },
     {
-        href: "/",
-        image: pw2,
-        title: "Conservatory Cleaning",
-        description: `We'll put a description of the job here`
+        image: dw3,
+        title: "roof clean in wheaton aston",
+        description: `The results speak for themselves! We had a lot of enquiries from this street after completion.`
     },
     {
-        href: "/",
-        image: pw1,
-        title: "Pressure Washing",
-        description: `We'll put a description of the job here`
+        image: rc3,
+        title: "roof clean in compton",
+        description: `Really proud of this one! A little too high and awkward for our scaffolding but nothing our Niftylift couldn't handle.`
     },
     {
-        href: "/",
-        image: wc3,
-        title: "Window Cleaning",
-        description: `We'll put a description of the job here`
+        image: rc2,
+        title: "roof clean in perton",
+        description: `The results speak for themselves! We had a lot of enquiries from this street after completion.`
     },
     {
-        href: "/",
-        image: gutter1,
-        title: "Gutter Cleaning",
-        description: `We'll put a description of the job here`
+        image: dw2,
+        title: "driveway in perton",
+        description: `A blocked driveway completely cleared and re-sanded. Another happy customer!`
     },
     {
-        href: "/",
-        image: wc1,
-        title: "Conservatory Cleaning",
-        description: `We'll put a description of the job here`
+        image: rc5,
+        title: "Roof Clean in Bridgenorth",
+        description: `Not only did we restore the condition of the tiles, we also replaced 14 tiles to make it look brand new!`
     },
     {
-        href: "/",
-        image: wc2,
-        title: "Window Cleaning",
-        description: `We'll put a description of the job here`
+        image: rc4,
+        title: "Roof Clean in Ludlow",
+        description: `Gentle wash and treatment here really brought out the original colour of the tiles. Simply amazing!`
     }
 ];
