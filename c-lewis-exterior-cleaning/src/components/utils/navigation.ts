ffb1d965a5532fe6f34d9dd3cd4ccf93a4d70584
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-07-31 19:44:03 +0100

    bug: clicking logo in nav would always scroll user to top of homepage, added setTimeout() to instead relocate them to the top of the homepage instead

--- c-lewis-exterior-cleaning/src/components/utils/navigation.ts
+++ c-lewis-exterior-cleaning/src/components/utils/navigation.ts
@@ -46,6 +46,9 @@ export const useLogoNavigation = () => {
             }
         } else {
             navigate('/');
+            setTimeout(() => {
+                window.scrollTo(0, 0);
+            }, 100);
         }
     };
 
