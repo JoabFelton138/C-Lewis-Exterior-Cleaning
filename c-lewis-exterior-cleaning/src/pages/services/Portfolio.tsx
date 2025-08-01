942614a71c33232057da5e441ce841e1b22e7a6f
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-08-01 15:18:58 +0100

    feat: useEffect to ensure we start at the top of the page when navogating from portfolio button click

--- c-lewis-exterior-cleaning/src/pages/services/Portfolio.tsx
+++ c-lewis-exterior-cleaning/src/pages/services/Portfolio.tsx
@@ -1,8 +1,12 @@
 import { Hero } from "@/components/shared/Hero";
 import { MasonryGrid } from "@/components/shared/MasonryGrid";
 import { SectionTitle } from "@/components/shared/SectionTitle";
+import {useEffect} from 'react';
 
 export const Portfolio = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
     return (
         <main>
             <Hero title="portfolio" subtitle="Our Work Speaks For Itself"/>
