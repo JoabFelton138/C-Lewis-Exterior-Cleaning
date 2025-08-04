149367aad0837af3cfaf65ab295c5d9c133ac763
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-08-04 13:12:42 +0100

    chore: added new images to portfolio

--- c-lewis-exterior-cleaning/src/data/portfolio-images.ts
+++ c-lewis-exterior-cleaning/src/data/portfolio-images.ts
@@ -1,12 +1,23 @@
 import rc1 from '../assets/images/before-after/after1.jpg';
-import dw1 from '../assets/images/portfolio/dw1.jpg';
 import rc2 from '../assets/images/portfolio/rc2.jpg';
 import rc3 from '../assets/images/before-after/roof-cleaning/roof-after-3.jpg';
 import rc4 from '../assets/images/portfolio/rc3.jpg';
 import rc5 from '../assets/images/portfolio/rc5.jpg';
+import rc6 from '../assets/images/portfolio/rc6.jpg';
+
+import dw1 from '../assets/images/portfolio/dw1.jpg';
 import dw2 from '../assets/images/portfolio/dw2.jpg';
 import dw3 from '../assets/images/portfolio/dw3.jpg';
+import dw4 from '../assets/images/portfolio/dw4.jpg';
+import dw5 from '../assets/images/portfolio/dw5.jpg';
 
+import gcwc from '../assets/images/portfolio/gcwc.jpg';
+import gc1 from '../assets/images/portfolio/gc1.jpg';
+import gc2 from '../assets/images/portfolio/gc2.jpg';
+
+import wc4 from '../assets/images/portfolio/wc4.jpg';
+
+import sp1 from '../assets/images/before-after/conservatory-and-solar-panel-cleaning/after.jpg'
 
 export const portfolioImages = [
     {
@@ -48,5 +59,85 @@ export const portfolioImages = [
         image: rc4,
         title: "Roof Clean in Ludlow",
         description: `Gentle wash and treatment here really brought out the original colour of the tiles. Simply amazing!`
+    },
+    {
+        image: gcwc,
+        title: "Gutter Clean in ?",
+        description: `?`
+    },
+    {
+        image: dw5,
+        title: "patio in ?",
+        description: `?`
+    },
+    {
+        image: rc6,
+        title: "roof clean in ?",
+        description: `?`
+    },
+    {
+        image: wc4,
+        title: "window clean in ?",
+        description: `?`
+    },
+    {
+        image: gc2,
+        title: "gutter clean in ?",
+        description: `?`
+    },
+    {
+        image: sp1,
+        title: "solar panel clean in ?",
+        description: `?`
+    },
+    {
+        image: dw4,
+        title: "patio clean in ?",
+        description: `?`
+    },
+    {
+        image: gc1,
+        title: "gutter Clean in ?",
+        description: `?`
+    },
+    {
+        image: rc1,
+        title: "Roof Clean in telford",
+        description: `Amazing outcome! Revealing the true colours of the tiling really boosted the properties appearance.`
+    },
+    {
+        image: dw1,
+        title: "driveway in wheaton aston",
+        description: `Pressure washed and re-sanded. This one was really slippery beforehand and quite a hazard due to the slope. Now nice and safe for a lovely cutomer!`
+    },
+    {
+        image: dw3,
+        title: "roof clean in wheaton aston",
+        description: `The results speak for themselves! We had a lot of enquiries from this street after completion.`
+    },
+    {
+        image: rc3,
+        title: "roof clean in compton",
+        description: `Really proud of this one! A little too high and awkward for our scaffolding but nothing our Niftylift couldn't handle.`
+    },
+    {
+        image: rc2,
+        title: "roof clean in perton",
+        description: `The results speak for themselves! We had a lot of enquiries from this street after completion.`
+    },
+    {
+        image: dw2,
+        title: "driveway in perton",
+        description: `A blocked driveway completely cleared and re-sanded. Another happy customer!`
+    },
+    {
+        image: rc5,
+        title: "Roof Clean in Bridgenorth",
+        description: `Not only did we restore the condition of the tiles, we also replaced 14 tiles to make it look brand new!`
+    },
+    {
+        image: dw4,
+        title: "patio clean in ?",
+        description: `?`
     }
 ];
