6149ac934362742d639632ce7c9c15351fdc3152
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-09-06 06:45:26 +0100

    chore: reduced hero image dimensions, adjusted section height for larger screens

--- c-lewis-exterior-cleaning/src/components/shared/Hero.tsx
+++ c-lewis-exterior-cleaning/src/components/shared/Hero.tsx
@@ -7,8 +7,7 @@ interface HeroProps {
 export const Hero = ({ title, subtitle, image }: HeroProps) => {
     
     return (
-        <section className="relative w-full h-[400px] sm:h-[500px] md:h-[600px] lg:h-[700px] xl:h-[800px]">
-            {image ? (
+        <section className="relative w-full h-[400px] sm:h-[500px] md:h-[550px] lg:h-[650px] xl:h-[750px]">            {image ? (
                 <img src={image} alt={title} className="w-full h-full object-cover" />
             ) : (
                 <div className="w-full h-full bg-gray-100">
