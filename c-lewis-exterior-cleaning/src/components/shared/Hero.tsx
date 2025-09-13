c93f816c9de09317a501a90c6b4efbdf8f3b95bd
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-09-13 08:30:05 +0100

    fix: adjusted h2 styles to scale better on smaller devices

--- c-lewis-exterior-cleaning/src/components/shared/Hero.tsx
+++ c-lewis-exterior-cleaning/src/components/shared/Hero.tsx
@@ -20,7 +20,7 @@ export const Hero = ({ title, subtitle, desktopImage, mobileImage }: HeroProps) 
             </picture>
             <div className="absolute inset-0 bg-black/30"></div>
             <div className="absolute inset-0 flex flex-col gap-1 sm:gap-2 md:gap-4 items-center justify-center">
-            <h2 className="text-[6vw] sm:text-3xl md:text-4xl lg:text-5xl xl:text-[65px] text-white title-style text-center">                    {title.toUpperCase()}
+            <h2 className="text-[5vw] sm:text-3xl md:text-4xl lg:text-5xl xl:text-[68px] whitespace-nowrap title-style text-white text-center">                    {title.toUpperCase()}
                 </h2>
                 <h4
                     className="text-[15px] sm:text-lg md:text-xl lg:text-2xl text-white title-style text-center"
