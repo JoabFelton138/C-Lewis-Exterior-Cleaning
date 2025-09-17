8257ac39dabf3c5fb7106f8c4fa1831510f695f3
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-09-17 18:00:10 +0100

    chore: converted images to webp where possible, reduced dimensions

--- c-lewis-exterior-cleaning/src/data/before-after/conservatoryImages.ts
+++ c-lewis-exterior-cleaning/src/data/before-after/conservatoryImages.ts
@@ -1,7 +1,7 @@
-import before1 from '../../assets/images/before-after/conservatory-and-solar-panel-cleaning/before.jpg'; 
-import after1 from '../../assets/images/before-after/conservatory-and-solar-panel-cleaning/after.jpg'; 
-import before2 from '../../assets/images/before-after/conservatory-and-solar-panel-cleaning/before1.jpg'; 
-import after2 from '../../assets/images/before-after/conservatory-and-solar-panel-cleaning/after1.jpg'; 
+import before1 from '../../assets/images/before-after/conservatory-and-solar-panel-cleaning/before.webp'; 
+import after1 from '../../assets/images/before-after/conservatory-and-solar-panel-cleaning/after.webp'; 
+import before2 from '../../assets/images/before-after/conservatory-and-solar-panel-cleaning/before1.webp'; 
+import after2 from '../../assets/images/before-after/conservatory-and-solar-panel-cleaning/after1.webp'; 
 
 export const conservatoryImages = [
     
--- c-lewis-exterior-cleaning/src/data/portfolio-images.ts
+++ c-lewis-exterior-cleaning/src/data/portfolio-images.ts
@@ -24,7 +24,7 @@ import wc2 from '../assets/images/portfolio/wc2.jpg';
 import wc3 from '../assets/images/portfolio/wc3.jpg';
 import wc4 from '../assets/images/portfolio/wc4.jpg';
 
-import sp1 from '../assets/images/before-after/conservatory-and-solar-panel-cleaning/after.jpg'
+import sp1 from '../assets/images/before-after/conservatory-and-solar-panel-cleaning/after.webp'
 import cc1 from '../assets/images/portfolio/cc7.jpg';
 
 export const portfolioImages = [
