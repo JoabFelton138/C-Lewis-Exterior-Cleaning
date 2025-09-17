82893c71270da6d72945712f6eb17e0b271361f4
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-09-17 17:48:38 +0100

    chore: converted images to webp where possible, reduced dimensions

--- c-lewis-exterior-cleaning/src/data/before-after/gutterImages.ts
+++ c-lewis-exterior-cleaning/src/data/before-after/gutterImages.ts
@@ -1,7 +1,7 @@
-import before1 from '../../assets/images/before-after/gutter-cleaning/before2.jpg';
-import after1 from '../../assets/images/before-after/gutter-cleaning/after-dirty.jpg';
-import before2 from '../../assets/images/before-after/gutter-cleaning/before1.jpg';
-import after2 from '../../assets/images/before-after/gutter-cleaning/after2.jpg';
+import before1 from '../../assets/images/before-after/gutter-cleaning/before2.webp';
+import after1 from '../../assets/images/before-after/gutter-cleaning/after-dirty.webp';
+import before2 from '../../assets/images/before-after/gutter-cleaning/before1.webp';
+import after2 from '../../assets/images/before-after/gutter-cleaning/after2.webp';
 
  export const gutterImages = [
     {before: before1, after: after1, beforeAlt: 'Dirty Gutters', afterAlt: 'Clean Gutters'},
