c5e8d020d0086962c088d8d1f911297ab7a5ddf2
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-09-25 13:25:35 +0100

    feat: placeholder images for windows before

--- c-lewis-exterior-cleaning/src/data/before-after/windowImages.ts
+++ c-lewis-exterior-cleaning/src/data/before-after/windowImages.ts
@@ -1,9 +1,10 @@
-import window1 from '../../assets/images/gallery/bwc.webp';
+import window1 from '../../assets/images/before-after/windows/windows-before-1.webp';
+import window2 from '../../assets/images/gallery/bwc.webp';
 import window3 from '../../assets/images/before-after/windows/window-before.webp';
 import window4 from '../../assets/images/before-after/windows/window-after.webp';
 
 
 export const windowImages = [
-    { before: window1, after: window1, beforeAlt: 'Before', afterAlt: 'After'},
+    { before: window1, after: window2, beforeAlt: 'Before', afterAlt: 'After'},
     { before: window3, after: window4, beforeAlt: 'Before', afterAlt: 'After'},
 ];
