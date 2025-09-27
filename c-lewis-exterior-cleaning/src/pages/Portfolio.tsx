d8a4f975bbd4222a124837ee960f42c675c500d5
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-09-27 20:28:07 +0100

    feat: new van image for hero in portfolio, replaced hero image in carousel for conservatory

--- c-lewis-exterior-cleaning/src/pages/Portfolio.tsx
+++ c-lewis-exterior-cleaning/src/pages/Portfolio.tsx
@@ -1,8 +1,8 @@
 import { Hero } from "@/components/shared/Hero";
 import { MasonryGrid } from "@/components/shared/MasonryGrid";
 import {useEffect} from 'react';
-import van from "@/assets/images/vans/van.webp";
-import vanMobile from "@/assets/images/vans/van-mobile.webp";
+import van from "@/assets/images/vans/new-van-2.webp";
+import vanMobile from "@/assets/images/vans/new-van-2-mobile-2.webp";
 
 export default function Portfolio() {
     useEffect(() => {
