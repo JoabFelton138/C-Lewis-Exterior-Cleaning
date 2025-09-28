6136281eeb45a2feeb96a02e6d0a073be2b5915a
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-09-28 21:58:11 +0100

    feat: updating content for window cleaning

--- c-lewis-exterior-cleaning/src/pages/services/WindowCleaning.tsx
+++ c-lewis-exterior-cleaning/src/pages/services/WindowCleaning.tsx
@@ -7,7 +7,7 @@ import { CtaBanner } from "@/components/services/CtaBanner";
 import { ServiceArea } from "@/components/services/ServiceArea";
 
 export default function WindowCleaning() {
-    const description1 = `Dirty window tracks collect dust, insects, and debris that can damage glass and trigger allergies. Professional cleaning protects your windows and keeps your home healthier — without the risk. With full insurance, specialist equipment, and training, we handle the hard-to-reach spots safely so you don’t have to!`;
+    const description1 = `Keeping your windows and frames clean is important because it enhances the overall appearance of your home or space, allowing more natural light to enter and creating a brighter environment. Regular cleaning also prevents the buildup of dirt, dust, and grime that can cause damage or deterioration over time. Additionally, clean windows and frames improve visibility and can help identify any potential issues, such as cracks or leaks, early on. Overall, maintaining clean windows and frames contributes to a healthier, more inviting, and well-maintained environment.`;
     const description2 = `We specialize in professional window cleaning using advanced, filtered water technology to deliver streak-free, sparkling results. 
                           Our eco-friendly approach ensures your windows are spotless and safe for your family and the environment. Trust us to brighten your 
                           home or business with a thorough, reliable service that leaves your windows crystal clear.`   
@@ -20,7 +20,7 @@ export default function WindowCleaning() {
                 mobileImage={windowHeroMobile} 
             />
             <ServiceLayout 
-                subtitle1="Why Clean My Windows?"
+                subtitle1="Maintaining Your Windows?"
                 description1={description1}
                 subtitle2="What's Included In The Service?"
                 description2={description2}
