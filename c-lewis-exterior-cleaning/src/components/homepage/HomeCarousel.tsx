b6e52957b2eae7aca0cb8c190e758cb06edade16
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-09-28 14:19:37 +0100

    feat: darkened image filter to help text stand out on hero / slides

--- c-lewis-exterior-cleaning/src/components/homepage/HomeCarousel.tsx
+++ c-lewis-exterior-cleaning/src/components/homepage/HomeCarousel.tsx
@@ -71,7 +71,7 @@ export const HomeCarousel = () => {
                   className="w-full h-full object-cover"
                  />
              </picture>
-              <div className="absolute inset-0 bg-black/30"></div>
+              <div className="absolute inset-0 bg-black/40"></div>
               <div className="absolute inset-0 flex items-center justify-center px-2 sm:px-4 md:px-6">
                 <div className="flex flex-col items-center gap-2 sm:gap-4 md:gap-6 px-2 sm:px-4">
                   <h2
