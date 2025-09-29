f3394f9dd33fbb2c46b95665e20e93a9efe4b721
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-09-29 12:59:56 +0100

    chore: removed fix attempt for layout shift

--- c-lewis-exterior-cleaning/src/components/shared/Hero.tsx
+++ c-lewis-exterior-cleaning/src/components/shared/Hero.tsx
@@ -8,17 +8,16 @@ interface HeroProps {
 export const Hero = ({ title, subtitle, desktopImage, mobileImage }: HeroProps) => {
     
     return (
-        <section className="relative w-full h-[85vh] sm:h-[70vh] md:h-[80vh] lg:h-[90vh] xl:h-screen overflow-hidden">            
-            <div className="hero-image-container bg-gray-200">
-                <picture>
-                    <source media="(max-width: 767px)" srcSet={mobileImage} />
-                    <source media="(min-width: 768px)" srcSet={desktopImage} />
-                    <img 
-                        src={desktopImage} 
-                        alt={title} 
-                    />
-                </picture>
-            </div>
+        <section className="relative w-full h-[85vh] sm:h-[70vh] md:h-[80vh] lg:h-[90vh] xl:h-screen">            
+            <picture>
+                <source media="(max-width: 767px)" srcSet={mobileImage} />
+                <source media="(min-width: 768px)" srcSet={desktopImage} />
+                <img 
+                    src={desktopImage} 
+                    alt={title} 
+                    className="w-full h-full object-cover"
+                />
+            </picture>
             <div className="absolute inset-0 bg-black/30"></div>
             <div className="absolute inset-0 flex flex-col gap-1 sm:gap-2 md:gap-4 items-center justify-center">
             <h2 className="text-[5vw] sm:text-3xl md:text-4xl lg:text-5xl xl:text-[68px] whitespace-nowrap title-style text-white text-center">                    {title.toUpperCase()}
