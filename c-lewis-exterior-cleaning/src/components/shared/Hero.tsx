5a76ebf1068495da5709e19d71a9f8f5bcd70081
JoabFelton138 <joab_felton91@hotmail.co.uk>
2025-09-29 17:02:12 +0100

    feat: applying same line clamping to h2 in Hero

--- c-lewis-exterior-cleaning/src/components/shared/Hero.tsx
+++ c-lewis-exterior-cleaning/src/components/shared/Hero.tsx
@@ -20,13 +20,12 @@ export const Hero = ({ title, subtitle, desktopImage, mobileImage }: HeroProps) 
             </picture>
             <div className="absolute inset-0 bg-black/30"></div>
             <div className="absolute inset-0 flex flex-col gap-1 sm:gap-2 md:gap-4 items-center justify-center">
-            <h2 className="text-[5vw] sm:text-3xl md:text-4xl lg:text-5xl xl:text-[68px] whitespace-nowrap title-style text-white text-center">                    {title.toUpperCase()}
+                <h2 className="text-[5vw] sm:text-3xl md:text-4xl lg:text-5xl xl:text-[68px] whitespace-nowrap title-style text-white text-center">
+                    {title.toUpperCase()}
                 </h2>
-                <h4
-                    className="text-[15px] sm:text-lg md:text-xl lg:text-2xl text-white title-style text-center"
-                  >
+                <h4 className="text-[15px] sm:text-lg md:text-xl lg:text-2xl text-white title-style text-center line-clamp-2 sm:line-clamp-none max-w-[320px] sm:max-w-[450px] md:max-w-[550px] lg:max-w-[650px] xl:max-w-[750px]">
                     {subtitle}
-                  </h4>
+                </h4>
             </div>
         </section>
     );
